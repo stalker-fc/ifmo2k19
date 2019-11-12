e94daea56e9868066896c012a36f55322ff1da13
sergey <stepanov.s@numdes.com>
2019-11-12 18:28:25 +0300

    add base struct

---
+++ labs/hamming.py
@@ -0,0 +1,24 @@
+"""Hamming code"""
+
+
+
+def generate_message(message_len: int):
+    pass
+
+
+
+
+def hamming_encode(message):
+    pass
+
+
+
+def add_noise(bytes_sequence):
+    pass
+
+
+
+
+def hamming_decode(message):
+    pass
+
