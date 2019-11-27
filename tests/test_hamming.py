c56c9eaae80ac4eca736a1c26405fe10adcb287c
sergey <stalker-fc@yandex.ru>
2019-11-27 17:38:42 +0300

    some tests

--- tests/test_hamming.py
+++ tests/test_hamming.py
@@ -2,7 +2,15 @@ from labs import hamming_decode, hamming_encode, hamming_set_error
 
 
 def test_set_error():
-    a = '11100111'
+    init = '11100111'
+    error = hamming_set_error(init)
+
+    assert init != error
+    assert len(init) == len(error)
+    assert all((c in ('0', '1') for c in error))
+
+    diff = sum([i != e for i, e in zip(init, error)])
+    assert diff == 1
 
 
 def test_encode():
