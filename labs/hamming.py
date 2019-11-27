571c8a27c7051e89bee897d921e162773b342b85
sergey <stepanov.s@numdes.com>
2019-11-27 16:05:07 +0300

    update

--- labs/hamming.py
+++ labs/hamming.py
@@ -8,6 +8,26 @@ def encode(message: str) -> str:
     data = message2bin(message)
 
 
+
+
+def kek(data):
+    data = insert_empty_parity_bits(data)
+
+    parity_bits_amount = len(data).bit_length()
+    parity_bits_indices = [(1 << i) - 1 for i in range(parity_bits_amount)]
+    parity_bits_values = [calc_parity_bit_value(data, count) for count in parity_bits_indices]
+
+
+    data = list(itertools.chain.from_iterable(data))
+
+    for idx, value in zip(parity_bits_indices, parity_bits_values):
+        data[idx] = value
+
+    print(''.join(data))
+
+
+
+
 def insert_empty_parity_bits(data: str) -> str:
     parity_bits_amount = len(data).bit_length()
     parity_bits_indices = [(1 << i) - 1 for i in range(parity_bits_amount)]
@@ -24,25 +44,17 @@ def insert_empty_parity_bits(data: str) -> str:
 
 def calc_parity_bit_value(data, parity_idx):
     start = parity_idx
-    block_len = 2 ** (parity_idx + 1)
+    deg = parity_idx.bit_length()
+    block = 2 ** deg
+    step = 2 ** (deg + 1)
 
     value = 0
-    for offset in range(block_len):
-        value += sum([int(data[i]) for i in range(start + offset, len(data), block_len)])
+    for offset in range(block):
+        value += sum([int(data[i]) for i in range(start + offset, len(data), step)])
 
     return str(value % 2)
 
-def kek(data):
-    data = insert_empty_parity_bits(data)
-
-    parity_bits_amount = len(data).bit_length()
-    parity_bits_indices = [(1 << i) - 1 for i in range(parity_bits_amount)]
-    parity_bits_values = [calc_parity_bit_value(data, idx) for idx in parity_bits_indices]
 
 
-    data = list(itertools.chain.from_iterable(data))
 
-    for idx, value in zip(parity_bits_indices, parity_bits_values):
-        data[idx] = value
 
-    print(''.join(data))
